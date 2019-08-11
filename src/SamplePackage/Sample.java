d0f7868179590e9b94ad3e87e741db0af76cd6cc
poornima S <poornii87@gmail.com>
2019-08-11 07:05:24 +0100

    HomeProject

---
+++ src/SamplePackage/Sample.java
@@ -0,0 +1,7 @@
+package SamplePackage;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("This is my HOME project with SAMPLE PACKAGE and SAMPLE CLASS");
+    }
+}
